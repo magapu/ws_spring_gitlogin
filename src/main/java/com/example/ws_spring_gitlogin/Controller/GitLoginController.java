2a0c61d8e2fd29dafa0f971112eab90e8be008e0
Srinivas Magapu <38003747+magapu@users.noreply.github.com>
2021-06-12 23:23:29 +0530

    Update GitLoginController.java
    Added Dto

--- src/main/java/com/example/ws_spring_gitlogin/Controller/GitLoginController.java
+++ src/main/java/com/example/ws_spring_gitlogin/Controller/GitLoginController.java
@@ -11,8 +11,13 @@ import java.security.Principal;
 public class GitLoginController {
 
     @GetMapping("/")
-    public String gitHubLogin(Principal principal){
-        log.info("user Data===> " + principal);
-        return  principal.getName();
+    @CrossOrigin
+    public UserDetailsDTO getData(OAuth2AuthenticationToken oAuth2AuthenticationToken){
+        log.info("User Info===> " + oAuth2AuthenticationToken.getPrincipal());
+        UserDetailsDTO userDetailsDTO = new UserDetailsDTO();
+        userDetailsDTO.setEmail(oAuth2AuthenticationToken.getPrincipal().getAttribute("email"));
+        userDetailsDTO.setUserName(oAuth2AuthenticationToken.getPrincipal().getAttribute("name"));
+        userDetailsDTO.setProfilePicture(oAuth2AuthenticationToken.getPrincipal().getAttribute("picture"));
+        return userDetailsDTO;
     }
 }
