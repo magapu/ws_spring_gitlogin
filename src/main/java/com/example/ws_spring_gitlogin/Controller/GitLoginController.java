29782a0d95279235fc06e4cad5d1b756d9cb92b6
magapu srinivas <mssrinivas7@gmail.com>
2021-06-12 23:34:51 +0530

    Pushing dto changes

--- src/main/java/com/example/ws_spring_gitlogin/Controller/GitLoginController.java
+++ src/main/java/com/example/ws_spring_gitlogin/Controller/GitLoginController.java
@@ -1,7 +1,9 @@
 package com.example.ws_spring_gitlogin.Controller;
 
 
+import com.example.ws_spring_gitlogin.DTO.UserDetailsDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -14,7 +16,7 @@ public class GitLoginController {
     @CrossOrigin
     public UserDetailsDTO gitHubLogin(OAuth2AuthenticationToken oAuth2AuthenticationToken){
         UserDetailsDTO userDetailsDTO = new UserDetailsDTO();
-        log.info("user Data===> " + oAuth2AuthenticationToken.getPrincipal());
+        log.info("user Login Data===> " + oAuth2AuthenticationToken.getPrincipal());
         userDetailsDTO.setEmail(oAuth2AuthenticationToken.getPrincipal().getAttribute("email"));
         userDetailsDTO.setUserName(oAuth2AuthenticationToken.getPrincipal().getAttribute("name"));
         return  userDetailsDTO;
